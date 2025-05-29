d914213dd9f1aa3ce2da3054655c540132380b8a
MahisaAghisni <aghisnifadhli@gmail.com>
2025-05-29 21:42:08 +0700

    flash message

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,12 +1,28 @@
 <script setup>
-import Navbar from '../Components/Navbar.vue';
+import { onMounted, reactive } from "vue";
+import * as Bootstrap from "bootstrap";
+import Navbar from "../Components/Navbar.vue";
+import Toast from "../Components/Toast.vue";
+
+const state = reactive({
+    toastRef: null,
+});
+
+onMounted(() => {
+    state.toastRef = new Bootstrap.Toast("#app-toast", {
+        autohide: true,
+        delay: 3000,
+    });
+});
+
+const showToast = () => state.toastRef.show();
 </script>
 
 <template>
-
     <Navbar />
-
+    <button class="btn btn-success" @click="showToast">Show Toast</button>
     <main class="py-4">
         <slot />
     </main>
+    <Toast id="app-toast" />
 </template>
